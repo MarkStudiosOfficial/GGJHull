f0790924ea8f32a05a0cd1ffc9a53b60970a63e9
Zqhx <arron.poucher@gmail.com>
2014-01-25 02:44:24 +0000

    Fixed box bug, now actually uses the colour specified.

--- box.py
+++ box.py
@@ -7,7 +7,7 @@ class Box( pygame.sprite.Sprite ):
         pygame.sprite.Sprite.__init__( self )
 
         self.image = pygame.Surface( size )
-        self.image.fill( white )
+        self.image.fill( colour )
 
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
