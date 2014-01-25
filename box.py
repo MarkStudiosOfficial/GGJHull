363374078adcaaec7c094c3c0675230a8cdeb56c
Zqhx <arron.poucher@gmail.com>
2014-01-25 05:42:22 +0000

    foobar

--- box.py
+++ box.py
@@ -1,15 +1,24 @@
 import pygame
+from colour import *
 
-from color import white
-
+# Box class, image-less sprite
 class Box( pygame.sprite.Sprite ):
     def __init__( self, colour, pos, size=(32,32) ):
+        # Subclass sprite
         pygame.sprite.Sprite.__init__( self )
 
+        # Create box
         self.image = pygame.Surface( size )
         self.image.fill( colour )
 
+        # Move to position
         self.rect = self.image.get_rect()
         self.rect.topleft = pos
-    def Move( self, deltaX, deltaY ):
-        self.rect = self.rect.move( deltaX, deltaY )
+
+    # Displace box by delta
+    def Move( self, delta ):
+        self.rect = self.rect.move( delta[0], delta[1] )
+
+    # Render to dest
+    def Render( self, dest ):
+        dest.blit( self.image, self.rect )
--- main.py
+++ main.py
@@ -2,45 +2,50 @@ import os, sys
 import pygame
 
 from box import *
-from image import *
-from spritesheet import *
-from color import *
+from colour import *
 
 def Main():
+    # Initialise PyGame
     pygame.init()
 
+    # Create window
     size = width, height = 640, 480
     screen = pygame.display.set_mode( size )
 
+    # Clock for FPS limit
     clock = pygame.time.Clock()
 
-    box1 = Box( blue, (20,20) )
-    img1 = Image( "stickman.bmp", (300,300), magicPink )
-    she1 = SpriteSheet( "spritesheet 3.bmp" )
-    bac1 = Box( blue, (300,300),(60,60) )
-    frame = she1.Next()
+    box1 = Box( white, screen.get_rect().center )
+    deltas = {  
+                pygame.K_UP:(0,-5),
+                pygame.K_DOWN:(0,5),
+                pygame.K_RIGHT:(5,0),
+                pygame.K_LEFT:(-5,0)
+            }
 
+    # Main loop
     while True:
+        # Event loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    pygame.quit()
-                    sys.exit()
-                if event.key == pygame.K_f:
-                    frame = she1.Next()
+                try:
+                    box1.Move( deltas[event.key] )
+                except KeyError:
+                    pass
 
+        # Limit FPS
         clock.tick( 60 )
+
+        # Clear screen
         screen.fill( orange )
-        screen.blit( box1.image, box1.rect )
-        screen.blit( img1.image, img1.rect )
-        screen.blit( frame, (100,400) )
-        screen.blit( she1.Image(1), (100,200) )
-        screen.blit( she1.Image(2), (100,242) )
-        screen.blit( she1.Image(3), (100,274) )
-        screen.blit( she1.Image(4), (100,316) )
+
+        # Render items
+        box1.Render( screen )
+
+        # Display changes
         pygame.display.flip()
 
 if __name__ == "__main__":
